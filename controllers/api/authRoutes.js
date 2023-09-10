0bfbc77da5bef73be23a3bb5d7beffa0162f04d4
Gil Rosario <grosario@alum.quinnipiac.edu>
2023-09-09 23:57:18 -0400

    update to route

--- controllers/api/authRoutes.js
+++ controllers/api/authRoutes.js
@@ -1,4 +1,3 @@
-// controllers/api/authRoutes.js
 const express = require('express');
 const router = express.Router();
 
@@ -7,15 +6,11 @@ router.get('/register', (req, res) => {
   res.render('auth/register');
 });
 
-// Register User (Implement your registration logic here)
 router.post('/register', async (req, res) => {
   try {
-    // Implement user registration logic here
-    // For example, create a new user in the database
     // Redirect to login page after successful registration
     res.redirect('/auth/login');
   } catch (error) {
-    // Handle registration errors, e.g., duplicate username
     res.render('auth/register', { error });
   }
 });
@@ -28,11 +23,8 @@ router.get('/login', (req, res) => {
 // Login User (Implement your login logic here)
 router.post('/login', async (req, res) => {
   try {
-    // Implement user login logic here
-    // Redirect to the user's dashboard after successful login
     res.redirect('/dashboard');
   } catch (error) {
-    // Handle login errors, e.g., invalid credentials
     res.render('auth/login', { error });
   }
 });
