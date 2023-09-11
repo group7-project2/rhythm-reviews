d81492213ecf5070d96f3dc6cd4ae73424f3ac21
Ashley Zemina <amzemina@gmail.com>
2023-09-10 22:36:35 -0400

    api fetch code, connected api to search bar

---
+++ public/js/search.js
@@ -0,0 +1,19 @@
+
+
+
+const url = 'https://theaudiodb.p.rapidapi.com/searchalbum.php?s=babymetal';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '3717db3bafmsh3630d39920bf588p1025c6jsnd065f1276f3c',
+		'X-RapidAPI-Host': 'theaudiodb.p.rapidapi.com'
+	}
+};
+
+// Ashley TODO #2: 
+// Once server-side APIs for audio db created, call them here
+// ex: albums = await fetch("/api/<artist search api>&artistName=<search value>")...
+// show "albums" variable value in html
+
+
+
