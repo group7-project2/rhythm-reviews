f1350066a66bef24573e6910c6df474da48c6610
Ashley Zemina <amzemina@gmail.com>
2023-09-16 14:13:59 -0400

    added code

--- config/connection.js
+++ config/connection.js
@@ -4,14 +4,14 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize();
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      // host: process.env.DB_HOST,
+      host: process.env.DB_HOST,
       dialect: 'mysql',
       port: 3306
     }
