847a21c0edf3bacd47c68535e71be86b16807a92
Ashley Zemina <amzemina@gmail.com>
2023-09-15 22:27:17 -0400

    formatted reviewRoutes

--- controllers/api/reviewRoutes.js
+++ controllers/api/reviewRoutes.js
@@ -4,43 +4,29 @@ const { Review, User } = require('../../models');
 const withAuth = require('../../utils/auth')
 const stylesPath = "../../../public/css/style.css"
 const { getAlbumsByArtist, getAlbumById } = require('../audioDB.js')
-// const getAlbumById = require('../audioDB.js')
-
-// const audioDbRootUrl = 'https://theaudiodb.p.rapidapi.com';
-// const audioDbOptions = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': '3717db3bafmsh3630d39920bf588p1025c6jsnd065f1276f3c',
-// 		'X-RapidAPI-Host': 'theaudiodb.p.rapidapi.com'
-// 	}
-// };
 
 router.get('/artist-search', async (req, res) => {
 
   const artistName = req.query.artistName;
 
-  
+
   if (!artistName || artistName.trim() === '') {
-   res.status(400).render('homepage', {stylesPath: stylesPath, message: 'Please enter a valid artist name.', logged_in: req.session.logged_in });
+    res.status(400).render('homepage', { stylesPath: stylesPath, message: 'Please enter a valid artist name.', logged_in: req.session.logged_in });
   }
 
-  try{
-  // const searchResult = await fetch(`${audioDbRootUrl}/searchalbum.php?s=${req.query.artistName}`, audioDbOptions)
-  // const albums = await searchResult.json()
-  const albums = await getAlbumsByArtist (req.query.artistName)
-  res.render('results', {stylesPath: stylesPath, albums: albums.album, logged_in: req.session.logged_in});
-} catch (error) {
-  res.status(500).render('homepage', {stylesPath: stylesPath, message: 'Error occured while fetching data.', logged_in: req.session.logged_in });
-}
-  
+  try {
+
+    const albums = await getAlbumsByArtist(req.query.artistName)
+    res.render('results', { stylesPath: stylesPath, albums: albums.album, logged_in: req.session.logged_in });
+  } catch (error) {
+    res.status(500).render('homepage', { stylesPath: stylesPath, message: 'Error occured while fetching data.', logged_in: req.session.logged_in });
+  }
+
 });
 
 router.get('/album/:id', async (req, res) => {
   try {
-    
-    // const response = await fetch(`${audioDbRootUrl}/album.php?m=${req.params.id}`, audioDbOptions)
-    // const album = await response.json()
-    const album = await getAlbumById (req.params.id)
+    const album = await getAlbumById(req.params.id)
     const reviews = await Review.findAll({
       where: {
         album_id: req.params.id
@@ -52,7 +38,7 @@ router.get('/album/:id', async (req, res) => {
         }
       ]
     });
-    res.render('review', {stylesPath: stylesPath, album: album.album, reviews: reviews, logged_in: req.session.logged_in})
+    res.render('review', { stylesPath: stylesPath, album: album.album, reviews: reviews, logged_in: req.session.logged_in })
   } catch (error) {
     console.log(error)
     res.status(500).send(error)
@@ -76,14 +62,14 @@ router.post('/create', withAuth, async (req, res) => {
 
 
 router.put('/update', withAuth, async (req, res) => {
-  try{
+  try {
     const updateReview = await Review.findByPk(req.body.id)
     updateReview.date = new Date()
     updateReview.title = req.body.title
     updateReview.content = req.body.content
     await updateReview.save();
     res.status(200).send()
-  }catch (error) {
+  } catch (error) {
     res.status(500).send(error)
   }
 })
