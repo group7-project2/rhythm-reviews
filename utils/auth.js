e64b32ee9d124838129bd25f762eeb6c71c9fe49
jenstem <124745888+jenstem@users.noreply.github.com>
2023-09-19 10:50:41 -0400

    Remove comments and reformat auth.js

--- utils/auth.js
+++ utils/auth.js
@@ -1,10 +1,9 @@
-// If the user is not logged in, redirect the request to the login route
 const withAuth = (req, res, next) => {
-    if (!req.session.logged_in) {
-      res.redirect('/login');
-    } else {
-      next();
-    }
-  };
+  if (!req.session.logged_in) {
+    res.redirect('/login');
+  } else {
+    next();
+  }
+};
 
-  module.exports = withAuth;
+module.exports = withAuth;
